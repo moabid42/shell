86020ff09a407189d899a3354ec2056c9f158705
Francesco Messina <iframe@live.it>
2022-09-08 10:36:26 +0200

    multiple unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: moabid <moabid@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 12:21:03 by frmessin          #+#    #+#             */
-/*   Updated: 2022/09/08 10:23:12 by moabid           ###   ########.fr       */
+/*   Updated: 2022/09/08 10:32:40 by moabid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,28 @@ static t_env    *remove_env_var(t_env *enviroment, t_env *node_to_erase)
 void ft_unset(char **argv, struct minishell *minishell)
 {
     t_env *tmp;
+    int i;
     
     printf("i'm here");
     tmp = minishell->env;
     if(!argv[1])
         write(1, "not enough arguments \n", 22);
     else
-        while(tmp)
+    {
+        while(argv[i])
         {
-            if(!my_strcmp(argv[1],tmp->name))
-                {
-                    printf("found %s", tmp->name);
-                    minishell->env = remove_env_var(minishell->env, tmp);
-                    exit (1);
-                }
-            tmp = tmp->next;
+            tmp = minishell->env;
+            while(tmp)
+            {
+                if(!my_strcmp(argv[1],tmp->name))
+                    {
+                        printf("found %s", tmp->name);
+                        minishell->env = remove_env_var(minishell->env, tmp);
+                        exit (1);
+                    }
+                tmp = tmp->next;
+            }
         }
+    }
     exit (0);
 }
