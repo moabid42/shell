5a818a2fe45ad82ec78d5c8cd14b5c7b26761fd7
Francesco Messina <frmessin@student.42heilbronn.de>
2022-10-23 22:02:28 +0200

    norminette

---
+++ src/utils/libft/ft_strtrim.d
@@ -0,0 +1,9 @@
+src/utils/libft/ft_strtrim.o: src/utils/libft/ft_strtrim.c \
+  src/utils/libft/libft.h src/utils/libft/get_next_line.h \
+  src/utils/libft/reader.h
+
+src/utils/libft/libft.h:
+
+src/utils/libft/get_next_line.h:
+
+src/utils/libft/reader.h:
