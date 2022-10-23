dac8148b9ff20b6c0224189d007470e875705d09
Francesco Messina <frmessin@student.42heilbronn.de>
2022-10-23 23:06:12 +0200

    fixed segfault open quotes

--- src/utils/libft/ft_putchar_fd.d
+++
@@ -1,9 +0,0 @@
-src/utils/libft/ft_putchar_fd.o: src/utils/libft/ft_putchar_fd.c \
-  src/utils/libft/libft.h src/utils/libft/get_next_line.h \
-  src/utils/libft/reader.h
-
-src/utils/libft/libft.h:
-
-src/utils/libft/get_next_line.h:
-
-src/utils/libft/reader.h:
--- src/utils/libft/reader_utils.d
+++
@@ -1,9 +0,0 @@
-src/utils/libft/reader_utils.o: src/utils/libft/reader_utils.c \
-  src/utils/libft/reader.h src/utils/libft/libft.h \
-  src/utils/libft/get_next_line.h
-
-src/utils/libft/reader.h:
-
-src/utils/libft/libft.h:
-
-src/utils/libft/get_next_line.h:
