707a2f5516be714cbd646dcf67c96b97bdac728b
Francesco Messina <iframe@live.it>
2022-10-26 01:01:21 +0200

    norme

--- src/utils/libft/ft_lstiter.d
+++
@@ -1,9 +0,0 @@
-src/utils/libft/ft_lstiter.o: src/utils/libft/ft_lstiter.c \
-  src/utils/libft/libft.h src/utils/libft/get_next_line.h \
-  src/utils/libft/reader.h
-
-src/utils/libft/libft.h:
-
-src/utils/libft/get_next_line.h:
-
-src/utils/libft/reader.h:
